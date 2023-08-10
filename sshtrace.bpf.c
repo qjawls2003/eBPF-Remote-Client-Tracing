4c354983617fe279933a567590ff1db0959bf635
abemelvin <abe.melvin@gmail.com>
2023-08-10 00:56:06 -0400

    woot

--- sshtrace.bpf.c
+++ sshtrace.bpf.c
@@ -16,56 +16,55 @@ struct {
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
-    __type(key, u32);
-    __type(value, struct msg_t);
-} my_config SEC(".maps");
+    __type(key, __u32);
+    __type(value, struct sockaddr_in *);
+}  values SEC(".maps");
 
-struct my_syscalls_enter_accept {
-   unsigned short common_type;
-   unsigned char common_flags;
-   unsigned char common_preempt_count;
-   int common_pid;
+static int probe_entry(void *ctx, struct sockaddr_in *addr)
+{
+   __u64 id = bpf_get_current_pid_tgid();
+   __u32 pid = id >> 32;
+   __u32 tid = (__u32)id;
 
-   int syscall_nr;
-   long fd;
-   struct sockaddr * upeer_sockaddr; //struct sockaddr *usockaddr;
-   int * upeer_addrlen;
+   bpf_map_update_elem(&values, &tid, &addr, BPF_ANY);
+   return 0;
 };
 
-
-SEC("tp/syscalls/sys_enter_accept")
-int tp_sys_enter_accept(struct my_syscalls_enter_accept *ctx)
+static int probe_return(void *ctx, int ret)
 {
-   
-   struct data_t data = {}; 
+   __u64 id = bpf_get_current_pid_tgid();
+   __u32 pid = id >> 32;
+   __u32 tid = (__u32)id;
+   __u32 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   struct sockaddr_in **addrpp;
+   struct sockaddr_in *addr;
+   struct data_t data = {};
 
-   //struct sockaddr_in *ip = (struct sockaddr_in *)ctx->usockaddr_ptr;
-   //bpf_printk("%ld %s\n", c_ip, "start");
-   //int c_ip = bpf_ntohl(&ip->sin_addr.s_addr);
-   //bpf_printk("%ld %s\n", c_ip, "start");
+   addrpp = bpf_map_lookup_elem(&values, &tid);
+   if (!addrpp)
+      return 0;
 
-   int err = bpf_probe_read_user(&data.client_ip, sizeof(data.client_ip), ctx->upeer_sockaddr);
-   if (err != 0) {
-      bpf_printk("Error");
-   }
-   
-   //bpf_printk("%ld\n", data.client_ip);
-
-   bpf_probe_read_kernel(&data.message, sizeof(data.message), tp_btf_exec_msg);
-   data.pid = bpf_get_current_pid_tgid() >> 32;
-   data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   addr = *addrpp;
+   data.pid = pid;
+   data.uid = uid;
+   data.ret = ret;
    bpf_get_current_comm(&data.command, sizeof(data.command));
-   
-   //bpf_probe_read(&data.client_ip, sizeof(data.client_ip), &ip->sin_addr.s_addr);
-
-   //bpf_printk("%ld\n", data.client_ip);
-   // TODO!! Resolve issues accessing data that isn't aligned to an 8-byte boundary
-   // bpf_printk("%s %d\n", tp_btf_exec_msg, pid);
-   // bpf_probe_read_kernel_str(&data.command, sizeof(data.command), ctx->pid); 
-
+   bpf_probe_read_user(&data.addr, sizeof(data.addr), addr);
    bpf_perf_event_output(ctx, &output, BPF_F_CURRENT_CPU, &data, sizeof(data));
-    
    return 0;
+}
+
+
+SEC("tp/syscalls/sys_enter_getpeername")
+int tp_sys_enter_getpeername(struct trace_event_raw_sys_enter *ctx)
+{
+   return probe_entry(ctx, (struct sockaddr_in *)ctx->args[1]);
+}
+
+SEC("tp/syscalls/sys_exit_getpeername")
+int tp_sys_exit_getpeername(struct trace_event_raw_sys_exit *ctx)
+{
+   return probe_return(ctx, (int)ctx->ret);
 }
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
